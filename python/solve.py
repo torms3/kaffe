2ffc5908617727e71bf53cdfb0d9beedb0f9b96b
torms3 <kisuklee@mit.edu>
2017-03-03 01:41:24 -0500

    update

--- python/solve.py
+++ python/solve.py
@@ -60,8 +60,8 @@ def run(gpu, cfg_path, async, last_iter=None):
     for i in net.inputs:
         net_spec[i] = net.blobs[i].data.shape[-3:]
         # Loss stats.
-        if '_mask' in i:
-            label, _ = i.split('_mask')
+        if '_label_mask' in i:
+            label, _ = i.split('_label_mask')
             loss[label] = 0.0
             nmsk[label] = 0.0
 
